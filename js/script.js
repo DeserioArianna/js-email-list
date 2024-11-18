74aaab49405f7bfb74109ce976d50c32c8613355
DeserioArianna <arianna.deserio@gmail.com>
2024-11-18 16:40:48 +0100

    feat:fine dell'esercizio

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const emailList = document.getElementById("email-list");
+// console.log(emailList)
+
+const emailGeneration = () => {
+    axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then(response => {
+        console.log(response);
+        console.log('Response data:', response.data); 
+        const email = response.data.response;
+        console.log('Email:', email); 
+        const listItem = document.createElement('li');
+        listItem.innerText = email;
+        emailList.appendChild(listItem);
+    })
+};
+
+for (let i = 0; i < 10; i++) {
+    emailGeneration();
+};
